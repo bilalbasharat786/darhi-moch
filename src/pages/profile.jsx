b4163f905e34fcce99ca3648baf1648bec96b1bb
bilalbasharat <hassanrohan366@gmail.com>
2025-07-02 07:32:14 +0100

    Implement email state management in Profile component

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, {useState}  from 'react'
 
 function Profile() {
+  const [user, setUser] = useState({
+    email:'',
+    password:''
+  })
+  const handleChangeEmail = (e)=> {
+    setUser({
+      ...user,
+      email: e.target.value
+    })
+  }
+ 
   return (
     <div className="container">
         <h1>login page</h1>
         <form className="row g-3 w-50 mt-4">
   <div className="col-md-12">
     <label htmlFor="inputEmail4" className="form-label">Email</label>
-    <input type="email" className="form-control" id="inputEmai"/>
+    <input type="email" className="form-control" onChange={handleChangeEmail} id="inputEmai"/>
   </div>
   <div className="col-md-12">
     <label htmlFor="password" className="form-label">Password</label>
-    <input type="password" className="form-control" id="password"/>
+    <input type="password" className="form-control"  id="password"/>
     
   </div>
   <div className="col-12">

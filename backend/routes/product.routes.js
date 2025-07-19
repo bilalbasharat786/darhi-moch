2ff42debc1843203883f6c845add5b630242cfd7
bilalbasharat <hassanrohan366@gmail.com>
2025-07-19 19:50:26 +0500

    Add .env file for database connection and refactor product routes for consistency

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,16 +1,12 @@
-import express from 'express'
-import {createNewProduct, getAllProducts, getProductById, updateProduct, deleteProduct} from '../controllers/product.controller.js'
+import express from 'express';
+import { createNewProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from '../controllers/product.controller.js';
 
-const app = express();
-const router = express.Router()
+const router = express.Router();
 
+router.route('/products/add').post(createNewProduct);
+router.route('/products').get(getAllProducts);
+router.route('/product/:name/:age/:qual').get(getProductById);
+router.route('/product/update').put(updateProduct);
+router.route('/product/delete').delete(deleteProduct);
 
-router.route('/products/add').post(createNewProduct)
-router.route('/products').get(getAllProducts)
-router.route('/product/:name/:age/:qual').get(getProductById)
-router.route('/product/update').put(updateProduct)
-router.route('/product/delete').delete(deleteProduct)
-
-
-
-export default router
+export default router;
